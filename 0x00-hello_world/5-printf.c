cfd4de0d68d41c813eeb47c9a19b123e28b6167d
ob-ose <oboseotoibhili@gmail.com>
2021-08-26 13:17:17 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ *Using the printf function
+ */
+
+#include <stdio.h>
+
+/**
+ * main - printing a line
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
