1d353bec050b5c203c080700a08a0544b3e87083
ob-ose <oboseotoibhili@gmail.com>
2021-08-26 13:13:41 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * This puts function prints without a new line character
+ */
+
+#include <stdio.h>
+
+/**
+ * main - block code
+ * Return: 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
